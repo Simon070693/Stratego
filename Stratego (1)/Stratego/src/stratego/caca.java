59f9b631756722febc54fbe53f619dc0392e1cdf
Sabbatini S <simonsabbatini@hotmail.com>
2014-12-03 15:01:20 +0100

    test github

---
+++ Stratego (1)/Stratego/src/stratego/caca.java
@@ -0,0 +1,23 @@
+package stratego;
+
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+
+public class caca extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public caca() {
+		
+		
+		JButton btnNewButton_1 = new JButton("New button");
+		add(btnNewButton_1);
+		
+		JButton btnNewButton = new JButton("New button");
+		add(btnNewButton);
+
+	}
+
+}
